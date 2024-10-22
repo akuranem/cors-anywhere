c43c040b52ddad8e110d14895a14ef8762d08a7a
akurazed <saadismeyas@gmail.com>
2024-10-22 22:26:54 +0100

    Added start script to package.json

--- server.js
+++ server.js
@@ -3,47 +3,34 @@ var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
-// Grab the blacklist from the command-line so that we can update the blacklist without deploying
-// again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
-// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
-// use originWhitelist instead.
-var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
-function parseEnvList(env) {
-  if (!env) {
-    return [];
-  }
-  return env.split(',');
-}
-
-// Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
-var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
-
+// Set up CORS Anywhere
 var cors_proxy = require('./lib/cors-anywhere');
+
+// Create and start the server
 cors_proxy.createServer({
-  originBlacklist: originBlacklist,
-  originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
-  checkRateLimit: checkRateLimit,
+  originBlacklist: [],  // Disable the blacklist (allow all origins)
+  originWhitelist: [],  // Disable the whitelist (allow all origins)
+  
+  // Remove the requirement for 'origin' and 'x-requested-with' headers
+  requireHeader: [],
+
+  // Remove headers like cookies to prevent forwarding sensitive information
   removeHeaders: [
     'cookie',
     'cookie2',
-    // Strip Heroku-specific headers
     'x-request-start',
     'x-request-id',
     'via',
     'connect-time',
-    'total-route-time',
-    // Other Heroku added debug headers
-    // 'x-forwarded-for',
-    // 'x-forwarded-proto',
-    // 'x-forwarded-port',
+    'total-route-time'
   ],
+
+  // Redirect same-origin requests
   redirectSameOrigin: true,
+
   httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-    xfwd: false,
-  },
+    xfwd: false,  // Disable forwarding headers (like X-Forwarded-For)
+  }
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
